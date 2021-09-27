3e78fff504aa8d741bb07e558b9016331b72bccc
AmrikSD <amrik.singh@live.co.uk>
2021-09-27 22:58:56 +0100

    ✅ Update Test to not be as dumb

--- src/test/java/de/amrik/MaximumNumberOfBalloonsTest.java
+++ src/test/java/de/amrik/MaximumNumberOfBalloonsTest.java
@@ -1,7 +1,8 @@
 package de.amrik;
 
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+
+import static org.junit.Assert.assertEquals;
 
 import static de.amrik.MaximumNumberOfBalloons.maximumNumberOfBalloons;
 
@@ -11,20 +12,20 @@ public class MaximumNumberOfBalloonsTest {
     public void balloonLettersAppearEachOnce() {
         String input = "nlaebolko";
         int expected = 1;
-        assertTrue((maximumNumberOfBalloons(input)) == expected);
+        assertEquals(expected, maximumNumberOfBalloons(input));
     }
 
     @Test
     public void balloonLettersAppearEachTwiceOnceInOrder() {
         String input = "loonbalxballpoon";
         int expected = 2;
-        assertTrue((maximumNumberOfBalloons(input)) == expected);
+        assertEquals(expected, maximumNumberOfBalloons(input));
     }
 
     @Test
     public void notAllBaloonLettersAppear() {
         String input = "leetcode";
         int expected = 0;
-        assertTrue((maximumNumberOfBalloons(input)) == expected);
+        assertEquals(expected, maximumNumberOfBalloons(input));
     }
 }
