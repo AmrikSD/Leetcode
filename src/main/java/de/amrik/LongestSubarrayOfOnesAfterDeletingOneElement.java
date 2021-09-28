f65c443f79f8b088db86e9ff76042e5241f8b202
AmrikSD <amrik.singh@live.co.uk>
2021-09-28 10:24:16 +0100

    🧪 Added test cases

---
+++ src/main/java/de/amrik/LongestSubarrayOfOnesAfterDeletingOneElement.java
@@ -0,0 +1,16 @@
+package de.amrik;
+
+public class LongestSubarrayOfOnesAfterDeletingOneElement {
+
+    /**
+     * Given a binary array nums, you should delete one element from it.
+     * 
+     * @return the size of the longest non-empty subarray containing only 1's in the
+     *         resulting array. Return 0 if there is no such subarray.
+     */
+    public static int getLengthOfSubarray(int[] nums) {
+
+        return 0;
+
+    }
+}
