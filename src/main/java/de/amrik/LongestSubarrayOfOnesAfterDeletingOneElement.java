572d4699896f3aef5ba86de0b85e65da792c5266
AmrikSD <amrik.singh@live.co.uk>
2021-09-28 14:01:50 +0100

    ✨ Solved in 1 pass I think (ish), messy logic though

--- src/main/java/de/amrik/LongestSubarrayOfOnesAfterDeletingOneElement.java
+++ src/main/java/de/amrik/LongestSubarrayOfOnesAfterDeletingOneElement.java
@@ -1,5 +1,7 @@
 package de.amrik;
 
+import java.util.Arrays;
+
 public class LongestSubarrayOfOnesAfterDeletingOneElement {
 
     /**
@@ -10,7 +12,35 @@ public class LongestSubarrayOfOnesAfterDeletingOneElement {
      */
     public static int getLengthOfSubarray(int[] nums) {
 
-        return 0;
+        // If we have all 0s in the array, return 0.
+        if (Arrays.stream(nums).sum() == 0) {
+            return 0;
+        }
+        if (Arrays.stream(nums).sum() >= nums.length - 1) {
+            return nums.length - 1;
+        }
+        int maxSeen = 0;
+        int left = 0;
+        int right = 0;
 
+        for (int i = 0; i < nums.length; i++) {
+            left += nums[i];
+
+            if (nums[i] == 0) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    right += nums[j];
+                    if (nums[j] == 0 || j == nums.length - 1) {
+                        i = j - 1;
+                        maxSeen = Math.max(maxSeen, left + right);
+                        left = right;
+                        right = 0;
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        return maxSeen;
     }
 }
