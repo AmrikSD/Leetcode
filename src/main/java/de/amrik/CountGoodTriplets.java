e9b99a5961eff591f99a97e080a838233d0ccda4
AmrikSD <Amrik.Singh@live.co.uk>
2022-03-13 20:56:39 +0000

    ✅ Finish count good triplets

---
+++ src/main/java/de/amrik/CountGoodTriplets.java
@@ -0,0 +1,25 @@
+package de.amrik;
+
+// this one is super boring and brute-foce only?
+public class CountGoodTriplets {
+    public static int countGoodTriplets(int[] arr, int a, int b, int c) {
+
+        int triplets = 0;
+
+        for (int i = 0; i < arr.length - 2; i++) {
+            for (int j = i + 1; j < arr.length - 1; j++) {
+                for (int k = j + 1; k < arr.length; k++) {
+                    System.out.println(i + "" + j + "" + k);
+                    if ((Math.abs(arr[i] - arr[j]) <= a) &&
+                            (Math.abs(arr[j] - arr[k]) <= b) &&
+                            (Math.abs(arr[i] - arr[k]) <= c)) {
+                        triplets++;
+                    }
+                }
+            }
+        }
+
+        return triplets;
+
+    }
+}
---
+++ src/test/java/de/amrik/CountGoodTripletsTest.java
@@ -0,0 +1,31 @@
+package de.amrik;
+
+import java.util.List;
+
+import static de.amrik.CountGoodTriplets.countGoodTriplets;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CountGoodTripletsTest {
+    @Test
+    public void zeroGoodTriplets() {
+
+        int[] arr = { 1, 1, 2, 2, 3 };
+        int a = 0, b = 0, c = 1;
+        int expected = 0;
+        int actual = countGoodTriplets(arr, a, b, c);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void fourAreFourGoodTriplets() {
+        int[] arr = { 3, 0, 1, 1, 9, 7 };
+        int a = 7, b = 2, c = 3;
+        int expected = 4;
+        int actual = countGoodTriplets(arr, a, b, c);
+
+        assertEquals(expected, actual);
+    }
+}
