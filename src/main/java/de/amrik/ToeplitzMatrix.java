94590d2395133485f30ab6ac30e13dc3d65374df
AmrikSD <Amrik.Singh@live.co.uk>
2022-03-13 21:49:18 +0000

    💡 add hint for this one, it took too long...

--- src/main/java/de/amrik/ToeplitzMatrix.java
+++ src/main/java/de/amrik/ToeplitzMatrix.java
@@ -4,6 +4,10 @@ public class ToeplitzMatrix {
 
     public static boolean isToeplitzMatrix(int[][] input) {
 
+        // instead of trying to go from [0,0] to [n,n] and check on a "per diagonal"
+        // basis,
+        // just check that each cell is equal to the one in it's "top left" - left and
+        // topmost ones...
         for (int i = 1; i < input.length; i++) {
             for (int j = 1; j < input[0].length; j++) {
                 if (input[i][j] != input[i - 1][j - 1]) {
