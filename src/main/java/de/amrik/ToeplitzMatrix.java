f9f5c028ddaa6b5694042986f25f2a76f614ecf7
AmrikSD <Amrik.Singh@live.co.uk>
2022-03-13 21:47:29 +0000

    ✅ Finish Toeplitz Matrix

--- src/main/java/de/amrik/ToeplitzMatrix.java
+++ src/main/java/de/amrik/ToeplitzMatrix.java
@@ -3,7 +3,16 @@ package de.amrik;
 public class ToeplitzMatrix {
 
     public static boolean isToeplitzMatrix(int[][] input) {
-        return false;
+
+        for (int i = 1; i < input.length; i++) {
+            for (int j = 1; j < input[0].length; j++) {
+                if (input[i][j] != input[i - 1][j - 1]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 
 }
